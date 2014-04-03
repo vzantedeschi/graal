80c1fa2c36819c341aa85c63ad3abf265e40156c
Aline Martin <aline.martin@insa-lyon.fr>
2014-04-03 02:57:37 +0200

    demo cyprien

--- ProcesseurXML/demoxml.sh
+++ ProcesseurXML/demoxml.sh
@@ -9,3 +9,13 @@ read r
 echo "Parsage d'un fichier XML dont un des éléments n'est pas fermé. Le fichier ne contient donc pas suffisamment de balises fermantes.
 "
 ./xmltool -p ./files/catalog_missing_markup.xml
+
+read r
+echo "Parsage d'un fichier XSL avec apply-templates
+"
+./xmltool -t ./files/catalog.xml ./files/catalog_apply_templates.xsl
+
+read r
+echo "Parsage d'un fichier XSL avec for-each
+"
+./xmltool -t ./files/catalog.xml ./files/catalog_foreach.xsl
